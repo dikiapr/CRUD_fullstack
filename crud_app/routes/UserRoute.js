a0184b7c5940b74284eca13d8d160fe65154efc4
Diki Apr <adiki436@gmail.com>
2022-11-18 10:55:35 +0700

    first commit

---
+++ crud_app/routes/UserRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getUsers, getUsersById, saveUser, updateUser, deletedUser } from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router.get("/users", getUsers);
+router.get("/users/:id", getUsersById);
+router.post("/users", saveUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deletedUser);
+
+export default router;
